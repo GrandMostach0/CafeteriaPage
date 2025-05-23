24afa34f1735fa6642f36ddd3726e2aed0d08b8b
GrandMostach0 <kreedlegend0@gmail.com>
2025-05-23 09:30:27 -0600

    Estructura Inicial del proyecto

---
+++ src/components/Layout.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+
+function Layout (){
+    return(
+        <h1>Soy un Layout.jsx</h1>
+    )
+}
+
+export default Layout;
---
+++ src/page/Home.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Home = () =>{
+    return(
+        <h1>SOY DEL HOME</h1>
+    )
+}
+
+export default Home;
